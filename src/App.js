85d425e0f202ee7c1b6aa7b9e94fda4287bf50d6
max <hennighausenmax@gmail.com>
2023-06-17 21:04:43 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Feed from './components/feed/Feed';
+
+function App() {
+  return (
+    <div className="App">
+      hello !!!
+      <Feed/>
+    </div>
+  );
+}
+
+export default App;
